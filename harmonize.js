b4805907ed95b451c2abd1642e89ac97b2cfcf32
Eric Chen <chen1994tw@gmail.com>
2015-07-12 17:02:56 -0400

    added functionality to payback live sound

--- harmonize.js
+++ harmonize.js
@@ -5,12 +5,41 @@ var musicBuffer = null;
 
 window.onload = function() {
   audioContext = new AudioContext();
-
   loadMusic('./music/find_your_soul.mp3');
 }
 
 function toggleLiveInput() {
-  // TODO: implement me!
+  getUserMedia({
+    "audio": {
+      "mandatory": {
+        "googEchoCancellation": "false",
+        "googAutoGainControl": "false",
+        "googNoiseSuppression": "false",
+        "googHighpassFilter": "false"
+      },
+      "optional": []
+    },
+  }, gotStream);
+}
+
+function getUserMedia(dictionary, callback) {
+  try {
+    navigator.getUserMedia = navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia;
+    navigator.getUserMedia(dictionary, callback, error);
+  } catch(e) {
+    alert('getUserMedia threw exception :' + e);
+  }
+}
+
+function gotStream(stream) {
+  var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+  mediaStreamSource.connect(audioContext.destination);
+}
+
+function error() {
+  alert('Stream generation failed.');
 }
 
 function loadMusic(url) {
