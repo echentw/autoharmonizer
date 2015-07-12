7462f7781a82ef946cb8f4ab510a0dc2c9f7c191
Eric Chen <chen1994tw@gmail.com>
2015-07-12 16:42:13 -0400

    added button to play music file

--- harmonize.js
+++ harmonize.js
@@ -1,11 +1,34 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = null;
+var musicBuffer = null;
 
 window.onload = function() {
   audioContext = new AudioContext();
+
+  loadMusic('./music/find_your_soul.mp3');
 }
 
 function toggleLiveInput() {
-
+  // TODO: implement me!
 }
+
+function loadMusic(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    audioContext.decodeAudioData(request.response, function(buffer) {
+      musicBuffer = buffer;
+    });
+  }
+  request.send();
+}
+
+function togglePlayMusic() {
+  var source = audioContext.createBufferSource();
+  source.buffer = musicBuffer;
+  source.connect(audioContext.destination);
+  source.start(0);
+}
+
